e5f84e3dc118cc7f362977db352ac1f3c52d9e87
Kevin Eslick <eslick.kevin@yahoo.com>
2020-08-29 11:40:07 -0500

    adding bootstrap and cards

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { Cards, Charts, Picker } from "./Components";
 import styles from './App.module.css'
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 function App() {
 
--- src/Components/Cards/Cards.js
+++ src/Components/Cards/Cards.js
@@ -1,13 +1,21 @@
 import React from "react";
 import useCards from "../../services/Cards.services";
+import Card from "react-bootstrap/Card";
 
 export default function Cards() {
     const confirmed = useCards()
 
 
     return (
-        <div>
-            {confirmed}
-        </div>
+        <Card border="dark" style={{ width: '10rem' }}>
+            <Card.Header>Header</Card.Header>
+            <Card.Body>
+                <Card.Title>{confirmed}</Card.Title>
+                <Card.Text>
+                    Some quick example text to build on the card title and make up the bulk
+                    of the card's content.
+                </Card.Text>
+            </Card.Body>
+        </Card>
     );
 };
